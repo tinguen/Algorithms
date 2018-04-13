119ef9588198b29054fade3f880d7053afc045f6
tinguen <tinguen@icloud.com>
2018-04-13 13:23:57 +0300

    added copy.cpp

--- Lab2/copy.cpp
+++ Lab2/copy.cpp
@@ -1,7 +1,18 @@
-Tree copy(Tree t1, Tree t2) { // make copy of tree t1 into t2
-    vector<int> arr = t1.to_array();
-    for (auto it = arr.begin(); it != arr.end(); it++) {
-        t2.push(*it);
+void copy_helper(Node *curr_t1, Node *prev_t2) { // prev_t2 - previous Node of tree t2, curr_t1 - current Node of tree t1
+    if (curr_t1->left) {
+        Node *element = new Node(curr_t1->left->data);
+        prev_t2->left = element;
+        copy_helper(curr_t1->left, element);
     }
+    if (curr_t1->right) {
+        Node *element = new Node(curr_t1->right->data);
+        prev_t2->right = element;
+        copy_helper(curr_t1->right, element);
+    }
+}
+
+Tree copy(Tree t1, Tree &t2) {
+    t2.push(t1.get_root()->data);
+    copy_helper(t1.get_root(), t2.get_root());
     return t2;
 }
